8c83d764350a6610590c14f8595b231fb79a2e3c
syedfaizz <syedfaizz45@gmail.com>
2021-08-06 20:13:01 +0530

    [faisal] Add: uc1-creat-stack

---
+++ stack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackandQueue
+{
+    class Stack
+    {
+        private Node top;
+        /// <summary>
+        /// constructor is used to initialized the top.
+        /// </summary>
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+        /// <summary>
+        /// add element into stack.
+        /// </summary>
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+                node.next = this.top;
+            else
+                node.next = this.top;
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack", value);
+        }
+        /// <summary>
+        /// display the stack elements.
+        /// </summary>
+        public void Display()
+        {
+            Node temp = this.top;
+            if (top == null)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
